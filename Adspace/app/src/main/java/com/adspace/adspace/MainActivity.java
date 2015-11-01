a31a50a56167683d5050cea0b911739819298793
Lenovo Yoga <Lenovo Yoga>
2015-11-01 19:19:14 +0100

    Android | Nejc: Dodan osnovni share fotografije na FB

--- Adspace/app/src/main/java/com/adspace/adspace/MainActivity.java
+++ Adspace/app/src/main/java/com/adspace/adspace/MainActivity.java
@@ -2,8 +2,6 @@ package com.adspace.adspace;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.media.Image;
-import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,46 +23,64 @@ import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.share.ShareApi;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareButton;
+import com.facebook.share.widget.ShareDialog;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity{
 
-    private TextView helloWorld;
-    Button btnTakPhoto;
-    ImageView imgTakenPhoto;
+    //GLOBALNE SPREMENLJIVKE
+    private TextView textViewLoginWelcome;
+    Bitmap thumbnail;
+    Button btnTakPhoto; //za zajem fotografije
+    ImageView imageViewposnetaFotografija; //thumbnail prikaz
     private static final int CAM_REQUEST = 1313;
     private CallbackManager mCallbackManager;
+    List<String> permissionNeeds = Arrays.asList("publish_actions");
+    ShareDialog shareDialog;
 
     private AccessTokenTracker mTokenTracker;
     private ProfileTracker mProfileTracker;
 
-
-
-
-
-    private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
+    //FACEBOOK FUNKCIJE
+    private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() { //callback manager za Login
         @Override
         public void onSuccess(LoginResult loginResult) {
-
             AccessToken acessToken = loginResult.getAccessToken();
             Profile profile = Profile.getCurrentProfile();
-            if (profile != null){
-                helloWorld.setText("Guten tag " + profile.getFirstName() );
-            }
-
-
+            displayWelcomeMassage(profile);
         }
 
         @Override
         public void onCancel() {
-
         }
 
         @Override
         public void onError(FacebookException e) {
-
         }
     };
+
+
+
+    private void sharePhotoToFacebook(Bitmap image){ //funkcija za deljenje fotografij
+        SharePhoto photo = new SharePhoto.Builder()
+                .setBitmap(image)
+                .setCaption("Adspace test")
+                .build();
+
+        SharePhotoContent content = new SharePhotoContent.Builder()
+                .addPhoto(photo)
+                .build();
+
+        ShareApi.share(content, null);
+    }
+//----------------------------------------------------------------------------------------------
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,33 +88,36 @@ public class MainActivity extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
 
-        helloWorld = (TextView) findViewById(R.id.textView);
-        mCallbackManager = CallbackManager.Factory.create();
-
-
-
-        mTokenTracker = new AccessTokenTracker() {
+        mTokenTracker = new AccessTokenTracker() { //upostevanje sprememb v fb racunu
             @Override
             protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
-
             }
         };
-
         mProfileTracker = new ProfileTracker() {
             @Override
             protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
-
             }
         };
         mTokenTracker.startTracking();
         mProfileTracker.startTracking();
 
+
+
+
+        Button shareButton =(Button) findViewById(R.id.buttonShare);
+        shareButton.setOnClickListener(new buttonShareOnClick());
+
+
+        textViewLoginWelcome = (TextView) findViewById(R.id.textView); //Pozdrav pri vpisu
+        mCallbackManager = CallbackManager.Factory.create();
+
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("user_friends");
+        //loginButton.setPublishPermissions("publish_actions");
         loginButton.registerCallback(mCallbackManager, mCallback);
 
-        btnTakPhoto = (Button) findViewById(R.id.buttonTakePicture);
-        imgTakenPhoto = (ImageView) findViewById(R.id.imageView);
+        btnTakPhoto = (Button) findViewById(R.id.buttonTakePicture); //gumb za zajem fotografije
+        imageViewposnetaFotografija = (ImageView) findViewById(R.id.imageView);  //prostor za prikaz fotografije
 
         btnTakPhoto.setOnClickListener(new btnTakePhotoClicker());
     }
@@ -107,20 +126,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == CAM_REQUEST && resultCode == RESULT_OK){ //rezultat za kamero
-            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-            imgTakenPhoto.setImageBitmap(thumbnail);
+            thumbnail = (Bitmap) data.getExtras().get("data");
+            imageViewposnetaFotografija.setImageBitmap(thumbnail);
         }
 
-        mCallbackManager.onActivityResult(requestCode,resultCode,data); //rezultat za FB login
+        mCallbackManager.onActivityResult(requestCode, resultCode, data); //rezultat za FB login
     }
 
+
+
+
+
+
+
+
+
     class btnTakePhotoClicker implements Button.OnClickListener{
-
-
         @Override
         public void onClick(View v) {
             Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(cameraIntent, CAM_REQUEST);
+        }
+    }
+
+    class buttonShareOnClick implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            System.out.println("Tu sem");
+            if (thumbnail != null){
+                Intent intent = new Intent(getApplicationContext(), ShareActivity.class);
+                intent.putExtra("picture", thumbnail );
+                startActivity(intent);
+            }
         }
     }
 
@@ -138,6 +175,22 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+
+    public void displayWelcomeMassage(Profile profile){ //ob vpisu se poklice ta metoda za prikaz imena
+    if(profile!= null){
+        textViewLoginWelcome.setText("Welcome to adspace " + profile.getFirstName());
+    }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        imageViewposnetaFotografija.setImageBitmap(thumbnail);
+        Profile profile = Profile.getCurrentProfile(); //Prikaz welcome massage
+        displayWelcomeMassage(profile);
+
     }
 
     @Override
